ae23e4aca04ad6bb2dd7c2de5515a169499ddce4
hoppe2808 <seb.hop@hotmail.com>
2016-10-11 15:27:57 +0200

    Added constructors and get/set methods to Date and Time classes

--- src/compiler/parsers/Time.java
+++ src/compiler/parsers/Time.java
@@ -2,4 +2,37 @@ package compiler.parsers;
 
 public class Time {
 
+	private int hours, minutes;
+	private double seconds;
+	
+	public Time(int hours, int minutes, double seconds){
+		this.hours = hours;
+		this.minutes = minutes;
+		this.seconds = seconds;
+	}
+
+	public int getHours() {
+		return hours;
+	}
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+	public double getSeconds() {
+		return seconds;
+	}
+
+	public void setSeconds(double seconds) {
+		this.seconds = seconds;
+	}
+	
 }
