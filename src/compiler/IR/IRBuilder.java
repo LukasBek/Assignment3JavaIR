5cc11f38c67de7d63e07acaa03bb2436aabf7507
Big Fat Llama <lukas@10.16.99.208>
2016-10-11 15:25:58 +0200

    Skriver i NWEntry

--- src/compiler/IR/IRBuilder.java
+++ src/compiler/IR/IRBuilder.java
@@ -1,10 +1,18 @@
 package compiler.IR;
 
+import java.util.LinkedList;
+
 public class IRBuilder {
 
 //	entries : entry *;
 //	entry : date time packet;
 	
-	public 
+	public NWEntry visitEntry(NetworkParser.NetworkContext ctx){
+		
+		LinkedList<NWEntry> networkHeaders = new LinkedList<NWEntry>();
+		networkHeaders.add(arg0)
+		
+		
+	}
 
 }
--- src/compiler/IR/NWEntry.java
+++ src/compiler/IR/NWEntry.java
@@ -1,5 +1,15 @@
 package compiler.IR;
 
 public class NWEntry {
+	
+	private Date date;
+	private Time time;
+	private Packet packet;
+	
+//	entry : date time packet;
+	
+	public NWEntry(Date date, Time time, Packet packet){
+		
+	}
 
 }
