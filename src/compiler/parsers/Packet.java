90eb5dac94c52b3e1d77abbac439f9925ab9db4a
Nicki N <nickinylin@hotmail.com>
2016-10-11 16:18:19 +0200

    pakcet update

--- src/compiler/parsers/Packet.java
+++ src/compiler/parsers/Packet.java
@@ -21,6 +21,54 @@ public class Packet {
 		this.ipv4Content = ipv4Content;
 		
 	}
+
+	public Mac getMac1() {
+		return mac1;
+	}
+
+	public void setMac1(Mac mac1) {
+		this.mac1 = mac1;
+	}
+
+	public Mac getMac2() {
+		return mac2;
+	}
+
+	public void setMac2(Mac mac2) {
+		this.mac2 = mac2;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public Length getLength() {
+		return length;
+	}
+
+	public void setLength(Length length) {
+		this.length = length;
+	}
+
+	public Ipv4Content getIpv4Content() {
+		return ipv4Content;
+	}
+
+	public void setIpv4Content(Ipv4Content ipv4Content) {
+		this.ipv4Content = ipv4Content;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
 	
 
 }
