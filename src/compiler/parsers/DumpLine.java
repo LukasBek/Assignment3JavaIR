0b4f97cfa54d82af44a42d9769f198089445e29a
hoppe2808 <seb.hop@hotmail.com>
2016-10-25 15:24:50 +0200

    Lavet de sidste parser klasser

--- src/compiler/parsers/DumpLine.java
+++ src/compiler/parsers/DumpLine.java
@@ -3,8 +3,10 @@ package compiler.parsers;
 public class DumpLine {
 
 	Hex hex;
+	String string;
 	
-	public DumpLine(Hex hex){
+	public DumpLine(Hex hex, String string){
 		this.hex = hex;
+		this.string = string;
 	}
 }
--- src/compiler/parsers/Word.java
+++ src/compiler/parsers/Word.java
@@ -2,4 +2,44 @@ package compiler.parsers;
 
 public class Word {
 
+	private Hex hex1, hex2, hex3, hex4;
+	
+	public Word(Hex hex1, Hex hex2, Hex hex3, Hex hex4){
+		this.hex1 = hex1;
+		this.hex2 = hex2;
+		this.hex3 = hex3;
+		this.hex4 = hex4;
+	}
+
+	public Hex getHex1() {
+		return hex1;
+	}
+
+	public void setHex1(Hex hex1) {
+		this.hex1 = hex1;
+	}
+
+	public Hex getHex2() {
+		return hex2;
+	}
+
+	public void setHex2(Hex hex2) {
+		this.hex2 = hex2;
+	}
+
+	public Hex getHex3() {
+		return hex3;
+	}
+
+	public void setHex3(Hex hex3) {
+		this.hex3 = hex3;
+	}
+
+	public Hex getHex4() {
+		return hex4;
+	}
+
+	public void setHex4(Hex hex4) {
+		this.hex4 = hex4;
+	}
 }
